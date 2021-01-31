master any fixes and add bleu score calculation

datamodules/wmt.py
@@ -7,6 +7,8 @@ import pytorch_lightning as pl
 
 import torch
 from torch.utils.data import DataLoader, Dataset, random_split
+from torchnlp.encoders.text.text_encoder import BatchedSequences
+
 from torchtext.utils import download_from_url, extract_archive, unicode_csv_reader
 
 import torchtext
@@ -193,6 +195,7 @@ class WMTDataModule(pl.LightningDataModule):
 
         self.bpe_tokenize()
 
+        # todo cache tokenized files
         src_data = self.tokenize_file(self.src_file, self.src_bpe)
         trg_data = self.tokenize_file(self.trg_file, self.trg_bpe)
 
@@ -212,10 +215,17 @@ class WMTDataModule(pl.LightningDataModule):
             trg_tokens_t: torch.LongTensor = torch.LongTensor(trg_tokens)
             trg_tensors.append(trg_tokens_t)
 
-        return stack_and_pad_tensors(src_tensors), stack_and_pad_tensors(trg_tensors)
+        src_trg_padded_stacked: BatchedSequences = stack_and_pad_tensors([*src_tensors, *trg_tensors])
+
+        src_padded: BatchedSequences = BatchedSequences( src_trg_padded_stacked.tensor[:len(src_tensors)], src_trg_padded_stacked.lengths[:len(src_tensors)] )
+        trg_padded: BatchedSequences = BatchedSequences( src_trg_padded_stacked.tensor[len(src_tensors):], src_trg_padded_stacked.lengths[len(src_tensors):] )
+
+        assert src_padded.tensor.size() == trg_padded.tensor.size()
+
+        return src_padded, trg_padded
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.wmt, batch_size=self.batch_size, collate_fn=self.collate_fn, shuffle=False)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.wmt, batch_size=self.batch_size)
+        return self.train_dataloader()

models/decoder.py
@@ -50,7 +50,7 @@ class TransformerDecoderBlock(nn.Module):
         decoder_hidden = self.masked_multihead_attention_add_norm(
             decoder_hidden, mask=trg_mask)
         encoder_decoder_outputs = self.encoder_decoder_block(
-            encoder_outputs, decoder_hidden, mask=trg_mask)
+            encoder_outputs, decoder_hidden, mask=src_mask)
 
         feed_forward_inputs = self.encoder_decoder_norm(
             decoder_hidden + encoder_decoder_outputs)

models/transformer.py
@@ -2,6 +2,8 @@ import math
 
 import torch
 import torch.nn as nn
+from torchnlp.utils import lengths_to_mask
+from torchnlp.encoders.text.text_encoder import BatchedSequences
 
 from models.encoder import TransformerEncoderBlock, EncoderBlocksSequential
 from models.decoder import TransformerDecoderBlock, DecoderBlocksSequential
@@ -76,7 +78,72 @@ class Transformer(nn.Module):
         decoder_output = self.decoder_blocks.forward(
             trg_embeddings, encoder_outputs, src_mask=src_mask, trg_mask=trg_mask)
 
-        return decoder_output
+        return decoder_output # batch_size x seq_len x hidden_dim
+
+    def _build_seq_eos_mask(self, tokens: torch.Tensor, eos_id=3, curr_pos_in_seq=0):
+        """
+        маскирует токены, которые идут после eos токена
+        """
+
+        current_max_seq_len = tokens.size(1)
+        lengths = []
+        for seq in tokens:
+            eos_indexes = torch.nonzero(seq == eos_id)
+            if eos_indexes.size(0) == 0:
+                lengths.append(current_max_seq_len)
+            else:
+                current_len = eos_indexes[0,0]
+                lengths.append(current_len)
+        assert len(lengths) == tokens.size(0)
+
+        mask: torch.Tensor = lengths_to_mask(lengths, device=tokens.device)
+        return mask
+
+
+    def encode_decode(self, src_batched_seq: BatchedSequences, src_pad_idx=0, trg_pad_idx=0, trg_bos_id=2, trg_eos_id=3, max_len=None):
+        """
+        Greedy decoder
+        """
+
+        src_tokens, src_le = src_batched_seq.tensor, src_batched_seq.lengths
+        src_mask: torch.Tensor = lengths_to_mask(src_batched_seq.lengths, device=src_tokens.device)
+
+        batch_size = src_tokens.size(0)
+        num_tokens_more = 10
+        if max_len is None:
+            max_len = src_mask.size(1) + num_tokens_more
+            num_tokens_more += src_mask.size(1) - src_tokens.size(1) # src_tokens и src_mask могут иметь разные размерности в seq_len!
+
+        src_extended_mask: torch.Tensor = torch.full((batch_size, max_len), False) # batch_size x max_len
+        src_extended_mask[:, :src_mask.size(1)] = src_mask
+
+        src_more_tokens_padding = torch.full((batch_size, num_tokens_more), src_pad_idx)
+        src_tokens = torch.cat((src_tokens, src_more_tokens_padding), dim=1) # batch_size x max_len
+
+        src_embeddings = self.input_embeddings(src_tokens)
+        encoder_outputs = self.encoder_blocks.forward(src_embeddings, src_mask=src_extended_mask)
+
+        # batch_size x max_len
+        trg_tokens = torch.full((batch_size, max_len), trg_pad_idx, device=src_tokens.device)
+        trg_tokens[:, 0] = trg_bos_id
+
+        trg_mask = torch.full_like(src_extended_mask, False)
+        for i in range(1, max_len):
+            trg_mask[:, i-1] = True
+
+            trg_embeddings = self.target_embeddings(trg_tokens)
+            decoder_output = self.decoder_blocks.forward(
+                trg_embeddings, encoder_outputs, src_mask=src_extended_mask, trg_mask=trg_mask)
+
+            # batch_size x trg_vocab_size
+            trg_tokens_probabilities = self.generator.forward(decoder_output)
+            _, next_tokens = torch.max(trg_tokens_probabilities, dim=1)
+
+            trg_tokens[:, i] = next_tokens
+
+        return trg_tokens
+
+
 
 
 class TransformerGenerator(nn.Module):

pl_transformer.py
@@ -1,6 +1,8 @@
 import os, math, random
 from argparse import ArgumentParser
+from nltk.translate.bleu_score import corpus_bleu
 
+import youtokentome as yttm
 import pytorch_lightning as pl
 import torch
 from torchnlp.encoders.text.text_encoder import BatchedSequences
@@ -10,8 +12,7 @@ from models import transformer
 
 
 class TransformerLightningModule(pl.LightningModule):
-    def __init__(
-        self,
+    def __init__(self,
         src_vocab_size: int,
         trg_vocab_size: int,
         hidden_dim: int=256,
@@ -21,11 +22,12 @@ class TransformerLightningModule(pl.LightningModule):
         smoothing: float = 0.1,
         lr: float = 1e-4,
         noam_opt_warmup_steps: int= 4000,
+        trg_bpe=None,
     ):
 
         super(TransformerLightningModule, self).__init__()
 
-        self.save_hyperparameters()
+        self.save_hyperparameters("src_vocab_size", "trg_vocab_size", "hidden_dim", "num_blocks", "key_query_value_dim", "padding_token_idx", "smoothing", "lr", "noam_opt_warmup_steps")
 
         self.transformer = transformer.Transformer(src_vocab_size, trg_vocab_size, hidden_dim,
                                                    num_blocks=num_blocks,
@@ -33,31 +35,33 @@ class TransformerLightningModule(pl.LightningModule):
 
         self.criterion = transformer.LabelSmoothing(trg_vocab_size, padding_token_idx=padding_token_idx, smoothing=smoothing)
 
+        self.trg_bpe: yttm.BPE = trg_bpe
+
         return
 
-
     def training_step(self, batch, batch_idx):
-        src_padded_tokens: BatchedSequences
-        trg_padded_tokens: BatchedSequences
-        src_padded_tokens, trg_padded_tokens = batch
+        src_batched_seq: BatchedSequences
+        trg_batched_seq: BatchedSequences
+        src_batched_seq, trg_batched_seq = batch
 
-        src_tokens: torch.Tensor = src_padded_tokens.tensor
-        src_mask: torch.Tensor = lengths_to_mask(src_padded_tokens.lengths, device=src_tokens.device)
+        src_tokens: torch.Tensor = src_batched_seq.tensor
+        _src_mask: torch.Tensor = lengths_to_mask(src_batched_seq.lengths, device=src_tokens.device)
+        src_mask = torch.full_like(src_tokens, False, device=src_tokens.device)
+        src_mask[:, :_src_mask.size(1)] = _src_mask
 
-        assert src_mask.size() == src_padded_tokens.tensor.size()
-
-        trg_tokens: torch.Tensor = trg_padded_tokens.tensor
-        trg_mask: torch.Tensor = lengths_to_mask(trg_padded_tokens.lengths, device=src_tokens.device)
+        trg_tokens: torch.Tensor = trg_batched_seq.tensor
+        _trg_mask: torch.Tensor = lengths_to_mask(trg_batched_seq.lengths, device=trg_tokens.device)
+        trg_mask = torch.full_like(trg_tokens, False, device=trg_tokens.device)
+        trg_mask[:, :_trg_mask.size(1)] = _trg_mask
         # целевые токены будем определять так:
         # просто рандомное число, которое будет меньше seq_len'а каждого предложения.
         # с учетом этого и будем формировать маску
         target_token_positions = []
-        for i, seq_len in enumerate(list(trg_padded_tokens.lengths.numpy())):
+        for i, seq_len in enumerate(list(trg_batched_seq.lengths.numpy())):
             assert seq_len > 1
             visible_length = random.randint(1, seq_len-1)
             trg_mask[i, visible_length:] = False
 
-        # todo how to determine target token?
         target_token_idxs: torch.Tensor = trg_tokens[torch.arange(trg_tokens.size(0)), target_token_positions]
 
         transformer_output = self.transformer.forward(src_tokens, trg_tokens, src_mask=src_mask, trg_mask=trg_mask)
@@ -69,19 +73,51 @@ class TransformerLightningModule(pl.LightningModule):
 
         return loss
 
+    # todo dropout
     def validation_step(self, batch, batch_idx):
-        pass
 
+        src_batched_seq: BatchedSequences
+        trg_batched_seq: BatchedSequences
+        src_batched_seq, trg_batched_seq = batch
+
+        src_tokens: torch.Tensor = src_batched_seq.tensor
+        trg_tokens: torch.Tensor = trg_batched_seq.tensor
+
+        translation = self.transformer.encode_decode(src_batched_seq)
+
+        ignore_ids = [0,1,2,3]
+        translation = translation.detach().cpu().numpy().tolist()
+        translation = self.trg_bpe.decode(translation, ignore_ids=ignore_ids)
+        target = trg_batched_seq.tensor.detach().cpu().numpy().tolist()
+        target = self.trg_bpe.decode(target, ignore_ids=ignore_ids)
+
+        translation_str = "\n".join(translation)
+        target_str = "\n".join(target)
+        self.logger.experiment.add_text("translate_decoded", translation_str)
+        self.logger.experiment.add_text("translate_target", target_str)
+
+        return translation_str, target_str
+
+    def validation_epoch_end(self, validation_step_outputs):
+        generated = []
+        references = []
+        for vout in validation_step_outputs:
+            generated.append(vout[0])
+            references.append([vout[1]])
+
+        calculated_bleu = corpus_bleu(references, generated)
+        self.log("valid_bleu", calculated_bleu)
+        return
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
 
-        parser.add_argument("--src_vocab_size", type=int, help="")
-        parser.add_argument("--trg_vocab_size", type=int, help="")
-        parser.add_argument("--hidden_dim", type=int, help="")
-        parser.add_argument("--num_blocks", type=int, help="")
-        parser.add_argument("--key_query_value_dim", type=int, help="")
+        parser.add_argument("--src_vocab_size", type=int)
+        parser.add_argument("--trg_vocab_size", type=int)
+        parser.add_argument("--hidden_dim", type=int)
+        parser.add_argument("--num_blocks", type=int)
+        parser.add_argument("--key_query_value_dim", type=int)
 
         parser.add_argument("--lr", type=float)
 
@@ -108,22 +144,27 @@ def cli_main(args=None):
     parser = ArgumentParser()
 
     # todo support other datamodules
-    dm_cls = WMTDataModule()
 
     parser = TransformerLightningModule.add_model_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args(args)
 
-    dm = dm_cls.from_argparse_args(args)
-    dm.download_dataset()
-    dm.bpe_tokenize()
-
+    dm = WMTDataModule()
+    dm.setup()
 
     if args.max_steps == -1:
         args.max_steps = None
 
-    transformer_model = TransformerLightningModule(dm.src_bpe.vocab_size(), dm.trg_bpe.vocab_size(), 512)
+    args.src_vocab_size = dm.src_bpe.vocab_size()
+    args.trg_vocab_size = dm.trg_bpe.vocab_size()
 
+    transformer_model = TransformerLightningModule(args.src_vocab_size, args.trg_vocab_size,
+                                                   hidden_dim=args.hidden_dim,
+                                                   num_blocks=args.num_blocks,
+                                                   key_query_value_dim=args.key_query_value_dim,
+                                                   trg_bpe=dm.trg_bpe)
+
+    print("training")
     trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(transformer_model, datamodule=dm)
     return dm, transformer_model, trainer