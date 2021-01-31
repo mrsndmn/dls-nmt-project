21d718e47aa3127d08357480251540f740b6ea9a
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2021-02-01 02:05:01 +0300

    want to log lr

--- pl_transformer.py
+++ pl_transformer.py
@@ -134,7 +134,7 @@ class TransformerLightningModule(pl.LightningModule):
         min_inv_sqrt = min(1/math.sqrt(current_step+1), current_step / math.sqrt(self.hparams.noam_opt_warmup_steps))
         current_lr = min_inv_sqrt / math.sqrt(self.hparams.hidden_dim)
 
-        self.log("lr", current_lr)
+        self.logger.experiment.add_scalar("lr", current_lr, self.current_epoch)
         return current_lr
 
     def configure_optimizers(self):
